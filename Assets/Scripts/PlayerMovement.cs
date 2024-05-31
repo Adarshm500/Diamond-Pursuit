2cd8a067cfe43745f0754de6f736bf08e3b368e5
Adarsh Maurya <adarshmaurya500@gmail.com>
2024-05-31 11:00:49 +0530

    added player animations
    Added player Idle animation,
    Added player walk animation
    Added player jump animation
    player first takes a stance and then jump after a wait time of around 0.13s to give a better jump feel

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,18 +9,50 @@ public class PlayerMovement : MonoBehaviour
     private float horizontalMove;
     private bool jump = false;
 
+    private bool isWalking;
+    private bool isJumping;
+
+    private float jumpTimer = 0;
+
     private void Update()
     {
+        if (isJumping)
+        {
+            jumpTimer += Time.deltaTime;
+            Debug.Log(jumpTimer);
+        }
+
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        isWalking = horizontalMove != 0;
 
         if (Input.GetButtonDown("Jump"))
-            {
+        {
+            isJumping = true;
+        }
+
+        if (jumpTimer >= 0.133) 
+        { 
             jump = true;
-            }
+        }
     }
     private void FixedUpdate()
     {
         characterController.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
-        jump = false;
+        if (isJumping && jumpTimer >= 0.14)
+        {
+            jump = false;
+            isJumping = false;
+            jumpTimer = 0;
+        }
+    }
+
+    public bool IsWalking()
+    {
+        return isWalking;
+    }
+
+    public bool IsJumping()
+    {
+        return isJumping;
     }
 }
